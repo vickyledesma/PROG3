14c346a13d6082df5df46fca96d340faf8771275
vickyledesma <112107886+vickyledesma@users.noreply.github.com>
2024-05-19 11:36:13 -0300

    Add files via upload

---
+++ Ejrcicio7.java
@@ -0,0 +1,35 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Ejrcicio7{
+    private List<List<Integer>> CaminosAlternativos;
+    private List<Integer> CaminoActual;
+
+    public List<List<Integer>> Caminos (GrafoNoDirigido g, int origen, int destino){
+        CaminosAlternativos = new ArrayList<>();
+        CaminoActual = new ArrayList<>();
+        buscarCaminos(g, origen, destino);
+        return CaminosAlternativos;
+    }
+
+
+    private void buscarCaminos(GrafoNoDirigido g, int origen, int destino) {
+        CaminoActual.add(origen);
+        if (origen == destino) {
+            CaminosAlternativos.add(new ArrayList<>(CaminoActual));
+        } else {
+            Iterator<Integer> adyacentes = g.obtenerAdyacentes(origen);
+            while (adyacentes.hasNext()) {
+                int vecino = adyacentes.next();
+                // Verifica si el tramo actual está cortado
+                if (!(origen == 4 && vecino == 5 || origen == 5 && vecino == 4) && !CaminoActual.contains(vecino)) {
+                    buscarCaminos(g, vecino, destino);
+                }
+            } 
+        }
+        CaminoActual.remove(CaminoActual.size() - 1);
+    }
+
+}
